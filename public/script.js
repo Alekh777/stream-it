57efc822ba594c8c0f1f94f84b1b0f5f69646a94
Alekh Kumar <alekhkumar777@gmail.com>
2022-05-08 23:01:16 +0530

    changes

--- public/script.js
+++ public/script.js
@@ -1,27 +1,47 @@
 let socket = io();
-let btn = document.getElementById("btn");
 let video = document.querySelector("video");
-let btnPlay = document.getElementById('btnPlay');
-let btnPause = document.getElementById('btnPause');
+let btnPlay = document.getElementById("btnPlay");
+let btnPause = document.getElementById("btnPause");
 
-btnPlay.onclick = function(){
-    console.log('play');
-    socket.emit('play');
+function seeked(){
+    let currTime = video.currentTime;
+    let seeking = true;
+    socket.emit("timestamp", { currTime, seeking });
+    console.log('seeked')
 }
 
-btnPause.onclick = function(){
-    console.log('pause')
-    socket.emit('pause');
-}
+video.addEventListener("play", () => {
+    console.log("play");
+    seeked();
+    socket.emit("play");
+});
 
-socket.on('pause', () => {
-    console.log('paused')
-    video.pause();
-})
+video.addEventListener("pause", () => {
+    console.log("pause")
+    seeked();
+    socket.emit("pause");
+});
 
-socket.on('play', () => {
-    console.log('played')
-    video.play();
+socket.on("pause", () => {
+    video.pause()
+    console.log("paused");
+    let currTime = video.currentTime;
+    socket.emit("timestamp", { currTime });
+});
+
+socket.on("play", () => {
+    video.play()
+    .then(() => {
+        console.log('PLAYED');
+    })
+    console.log("played");
+});
+
+// sync video
+socket.on('sync-video', (data) => {
+    console.log('previous time', video.currentTime);
+    video.currentTime = data.min;
+    console.log('updated time', video.currentTime);
 })
 
 document.getElementById("videoUpload").onchange = function (event) {
--- server.js
+++ server.js
@@ -6,12 +6,38 @@ const app = express()
 const server = http.createServer(app);
 const io = socketio(server)
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 7777;
 
 app.use('/', express.static(__dirname + '/public'))
 
+let timeMap = {};
+let users = 0;
+let pausedUsers = 0;
+
 io.on('connection', (socket) => {
     console.log('connection ', socket.id)
+    users++;
+    console.log('users', users)
+
+    function minTime(){
+        console.log('called');
+        console.log('paused users', pausedUsers);
+        if(pausedUsers === users){
+            console.log(timeMap);
+            const values = Object.values(timeMap);
+            console.log('values', values);
+            let min = Math.min(...values);
+            console.log('min', min)
+            io.emit('sync-video', {min});
+            pausedUsers = 0;
+        }
+    }
+
+    socket.on('disconnect', () => {
+        console.log('disconnected', socket.id);
+        delete timeMap[socket.id];
+        users--;
+    })
 
     socket.on('play', () => {
         console.log('played')
@@ -20,10 +46,26 @@ io.on('connection', (socket) => {
 
     socket.on('pause', () => {
         console.log('paused')
-        io.emit('pause')
+        io.emit('pause');
+        pausedUsers++;
+    })
+    
+    socket.on('timestamp', (data) => {
+        if(data.seeking){
+            let min = data.currTime;
+            io.emit('sync-video', {min});
+            return;
+        }
+        let currTime = data.currTime;
+        console.log(currTime, socket.id);
+        let id = socket.id
+        console.log('users inside', users)
+        timeMap[socket.id] = currTime;
+        minTime();
+        console.log('pausedUsers inside', pausedUsers);
     })
 })
 
 server.listen(port, ()=>{
-    console.log(`listening at http://localhost:4444`);
+    console.log(`listening at http://localhost:${port}`);
 })
