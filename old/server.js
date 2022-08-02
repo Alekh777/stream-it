29fa2a53811bd4a7d96e47c040ef868e94ad1956
Alekh Kumar <alekhkumar777@gmail.com>
2022-08-02 23:43:24 +0530

    minor change

--- old/server.js
+++ old/server.js
@@ -1,64 +1,64 @@
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
-const app = express()
+const app = express();
 const server = http.createServer(app);
-const io = socketio(server)
+const io = socketio(server);
 
 const port = process.env.PORT || 7777;
 
-app.use('/', express.static(__dirname + '/public'))
+app.use("/", express.static(__dirname + "/public"));
 
 let timeMap = {};
 let users = 0;
 let pausedUsers = 0;
 
-io.on('connection', (socket) => {
-    console.log('connection ', socket.id)
+io.on("connection", (socket) => {
+    console.log("connection ", socket.id);
     users++;
-    console.log('users', users)
+    console.log("users", users);
 
-    function minTime(){
-        if(pausedUsers === users){
+    function minTime() {
+        if (pausedUsers === users) {
             const values = Object.values(timeMap);
             let min = Math.min(...values);
-            io.emit('sync-video', {min});
+            io.emit("sync-video", { min });
             pausedUsers = 0;
             timeMap = {};
         }
     }
 
-    socket.on('disconnect', () => {
-        console.log('disconnected', socket.id);
+    socket.on("disconnect", () => {
+        console.log("disconnected", socket.id);
         delete timeMap[socket.id];
         users--;
-    })
+    });
 
-    socket.on('play', () => {
-        console.log('played')
-        io.emit('play')
-    })
+    socket.on("play", () => {
+        console.log("played");
+        io.emit("play");
+    });
 
-    socket.on('pause', () => {
-        console.log('paused')
-        io.emit('pause');
-    })
-    
-    socket.on('timestamp', (data) => {
-        console.log('timestamp')
-        if(data.seeking){
+    socket.on("pause", () => {
+        console.log("paused");
+        io.emit("pause");
+    });
+
+    socket.on("timestamp", (data) => {
+        console.log("timestamp");
+        if (data.seeking) {
             let min = data.currTime;
-            io.emit('sync-video', {min});
+            io.emit("sync-video", { min });
             return;
         }
         pausedUsers++;
         let currTime = data.currTime;
         timeMap[socket.id] = currTime;
         minTime();
-    })
-})
+    });
+});
 
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
-})
+});
